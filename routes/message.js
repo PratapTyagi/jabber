fa4987ca809ebbc4ca14451c2ae09745c0b85834
Pratap Tyagi <tyagipratap111@gmail.com>
2021-08-07 23:35:43 +0530

    Authenticate user middleware to message routes

--- routes/message.js
+++ routes/message.js
@@ -4,7 +4,9 @@ const router = express.Router();
 
 import { createMessage, getMessage } from "../controllers/message.js";
 
-router.post("/sync", getMessage);
-router.post("/new", createMessage);
+import userAuthenticated from "../middlewares/userAuthenticated/userAuthenticated.js";
+
+router.post("/sync", userAuthenticated, getMessage);
+router.post("/new", userAuthenticated, createMessage);
 
 export default router;
